aaa7682216fb3fc64585fda2db7d5fb044c9ee06
c2kle <c2kle@live.unc.edu>
2022-11-06 01:13:44 -0500

    vertical pieces

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div id="home" className="home">
       <div className="home_section">        <h1 className="home_section_h1">ELITE TENNIS CONSULTING</h1></div>
+      <div className="home_section">        <h1 className="home_section_h1">TENNIS CONSULTING</h1></div>
 
 </div>
   )
