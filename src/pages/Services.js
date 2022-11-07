98ac671c6a4c5e068937284df8b0b65eaecb8f9f
c2kle <c2kle@live.unc.edu>
2022-11-06 20:37:16 -0500

    innerHeight/Width

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import '../pageStyles/services.css'
 
-export default function Services() {
+export default function Services(props) {
+
+
     return (
-        <div id="services" className="services">
-            <div className="services_section">
-            </div>
-            <div className="services_section">
-            </div>
-            <div className="services_section">
+        <div style={{height: `${props.innerHeight}px`, width: `${props.innerWidth}px`}} id="services" className="services">
+            <div className="services_wrapper">
+                <div className="services_wrapper_section">
+                    <h1 className="services_wrapper_section_h1">SERVICES</h1>
+                </div>
             </div>
         </div>
+
     )
 }
--- src/parentContainer/Main.js
+++ src/parentContainer/Main.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import Home from '../pages/Home'
 import About from '../pages/About'
 import Team from '../pages/Team'
@@ -9,14 +10,23 @@ import Members from '../pages/Members'
 import '../parentContainerStyles/main.css'
 import Navigation from '../components/Navigation'
 
-export default function main() {
+export default function Main() {
+
+  const [innerHeight,setInnerHeight] = useState(window.innerHeight)
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth)
+
+  window.addEventListener('resize', () => {
+    setInnerHeight(window.innerHeight)
+    setInnerWidth(window.innerWidth)
+  })
+
   return (
 
     <div className="main">
       <div className="main_header">
         <Navigation />
       </div>
-      <div className="main_body">
+      <div style={{height:  `${innerHeight - 52}px`, width:  `${innerWidth}px` }} className="main_body">
         <Home />
         <About />
         <Team />
