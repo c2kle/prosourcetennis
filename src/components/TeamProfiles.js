24de30cd4a2318900db7015c14991f99aca7d60e
c2kle <c2kle@live.unc.edu>
2022-11-11 18:20:36 -0500

    team back

--- src/components/TeamProfiles.js
+++ src/components/TeamProfiles.js
@@ -9,31 +9,33 @@ export default function TeamProfiles(props) {
 
     return (
         <div className="team-profiles-wrapper">
-            <div className="team-profiles-wrapper_section">
-                <div className="team-profiles-wrapper_section_h1">ATP #355</div>
-                <div className="team-profiles-wrapper_section_h1">3 Men's ITF Pro Titles (2S, 1D)</div>
-                <div className="team-profiles-wrapper_section_h1">3 x Junior Grandslam Competitor</div>
-                <div name="ashley-hewitt" className="team-profiles-wrapper_section_profle"></div>
-                <button className="team-profiles-wrapper_section_button" >Bio: Ashley Hewitt</button>
-            </div>
+            <div className="team-profiles-wrapper_body">
+                <div className="team-profiles-wrapper_body_section">
+                    <div className="team-profiles-wrapper_body_section_h1">ATP #355</div>
+                    <div className="team-profiles-wrapper_body_section_h1">3 Men's ITF Pro Titles (2S, 1D)</div>
+                    <div className="team-profiles-wrapper_body_section_h1">3 x Junior Grandslam Competitor</div>
+                    <div name="ashley-hewitt" className="team-profiles-wrapper_body_section_profle"></div>
+                    <button className="team-profiles-wrapper_body_section_button" >Bio: Ashley Hewitt</button>
+                </div>
 
-            <div className="team-profiles-wrapper_section">
-                <div className="team-profiles-wrapper_section_h1">Duke University #1</div>
-                <div className="team-profiles-wrapper_section_h1">NCAA Finalist</div>
-                <div className="team-profiles-wrapper_section_h1">Junior Grandslam Competitor</div>
-                <div name="nick-stachowiak" className="team-profiles-wrapper_section_profle"></div>
-                <button className="team-profiles-wrapper_section_button" >Bio: Nick Stachowiak</button>
-            </div>
+                <div className="team-profiles-wrapper_body_section">
+                    <div className="team-profiles-wrapper_body_section_h1">Duke University #1</div>
+                    <div className="team-profiles-wrapper_body_section_h1">NCAA Finalist</div>
+                    <div className="team-profiles-wrapper_body_section_h1">Junior Grandslam Competitor</div>
+                    <div name="nick-stachowiak" className="team-profiles-wrapper_body_section_profle"></div>
+                    <button className="team-profiles-wrapper_body_section_button" >Bio: Nick Stachowiak</button>
+                </div>
 
-            <div className="team-profiles-wrapper_section">
-                <div className="team-profiles-wrapper_section_h1">ATP #464</div>
-                <div className="team-profiles-wrapper_section_h1">5 Men's ITF Pro Titles (1S, 4D)</div>
-                <div className="team-profiles-wrapper_section_h1">Northwestern University #1</div>
-                <div name="strong-kirchheimer" className="team-profiles-wrapper_section_profle"></div>
-                <button className="team-profiles-wrapper_section_button" >Bio: Strong Kirchheimer</button>
+                <div className="team-profiles-wrapper_body_section">
+                    <div className="team-profiles-wrapper_body_section_h1">ATP #464</div>
+                    <div className="team-profiles-wrapper_body_section_h1">5 Men's ITF Pro Titles (1S, 4D)</div>
+                    <div className="team-profiles-wrapper_body_section_h1">Northwestern University #1</div>
+                    <div name="strong-kirchheimer" className="team-profiles-wrapper_body_section_profle"></div>
+                    <button className="team-profiles-wrapper_body_section_button" >Bio: Strong Kirchheimer</button>
+                </div>
             </div>
-            <div className="team-profiles-wrapper_nav">
-                <button className="team-profiles-wrapper_nav_button" onClick={onClickHandler}>Back</button>
+            <div className="team-profiles-wrapper_footer">
+                <div className="team-profiles-wrapper_footer_nav-button" onClick={onClickHandler}>Back</div>
             </div>
         </div>
     )
