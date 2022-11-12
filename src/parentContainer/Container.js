8dc6b0fae5c35dc1a0bf665123fc0cad07052fd9
c2kle <c2kle@live.unc.edu>
2022-11-12 18:50:00 -0500

    meta tag

--- src/parentContainer/Container.js
+++ src/parentContainer/Container.js
@@ -3,7 +3,7 @@ import '../parentContainerStyles/container.css'
 import Main from './Main';
 import Landing from '../pages/Landing';
 import { useState } from 'react';
-// import { useDebouncedCallback } from 'use-debounce'
+import { useDebouncedCallback } from 'use-debounce'
 
 export default function Container() {
 
@@ -15,9 +15,14 @@ export default function Container() {
   //innerWidth bug, can use outerWidth since browser toolbars are vertical
   
 
-  // const debouncedVersion = useDebouncedCallback(() => {
-  //   setInnerHeight(window.innerHeight)
-  // }, 20)
+  const debouncedVersion = useDebouncedCallback(() => {
+
+    document.getElementsByName("viewport")[0].remove()
+    let meta = document.createElement("meta")
+    meta.name = "viewport"
+    meta.content = "width=device-width, initial-scale=1"
+    document.getElementsByTagName("head")[0].appendChild(meta)
+  }, 20)
 
   // window.addEventListener('resize', debouncedVersion)
 
@@ -30,7 +35,10 @@ export default function Container() {
 
   window.addEventListener('orientationchange', () => {
     setInnerHeight(window.innerHeight)
+
   })
+
+  window.addEventListener('orientationchange', debouncedVersion)
 
 
   let bodyContent;
