b7592f7a581ec513592a8ac0461a89da122489b6
c2kle <c2kle@live.unc.edu>
2022-11-13 02:41:32 -0500

    no zoom

--- src/parentContainer/Container.js
+++ src/parentContainer/Container.js
@@ -5,10 +5,10 @@ import Landing from '../pages/Landing';
 import { useState } from 'react';
 // import { useDebouncedCallback } from 'use-debounce'
 
-export default function Container(props) {
+export default function Container() {
 
-  // const [innerHeight, setInnerHeight] = useState(window.innerHeight)
-  // const [innerWidth, setInnerWidth] = useState(window.innerWidth)
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight)
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth)
   const [landed,setLanded] = useState(false)
 
   //innerWidth bug, can use outerWidth since browser toolbars are vertical
@@ -23,22 +23,22 @@ export default function Container(props) {
 
   // window.addEventListener('orientationchange', debouncedVersion)
 
-  // window.addEventListener('resize', () => {
-  //   setInnerHeight(window.innerHeight)
-  // })
+  window.addEventListener('resize', () => {
+    setInnerHeight(window.innerHeight)
+  })
 
-  // window.addEventListener('resize', () => {
-  //   setInnerWidth(window.innerWidth)
-  // })
+  window.addEventListener('resize', () => {
+    setInnerWidth(window.innerWidth)
+  })
 
 
-  // window.addEventListener('orientationchange', () => {
-  //   setInnerHeight(window.innerHeight)
-  // })
+  window.addEventListener('orientationchange', () => {
+    setInnerHeight(window.innerHeight)
+  })
 
-  // window.addEventListener('orientationchange', () => {
-  //   setInnerWidth(window.innerWidth)
-  // })
+  window.addEventListener('orientationchange', () => {
+    setInnerWidth(window.innerWidth)
+  })
 
 
 
@@ -48,17 +48,17 @@ export default function Container(props) {
   let bodyContent;
 
   if (landed === true) {
-    // bodyContent = <Main innerHeight={props.innerHeight} />
-    bodyContent = <Main />
+    bodyContent = <Main innerHeight={innerHeight} />
+    // bodyContent = <Main />
   }
   else {
-    // bodyContent = <Landing innerHeight={props.innerHeight} setLanded={setLanded} />
-    bodyContent = <Landing setLanded={setLanded} />
+    bodyContent = <Landing innerHeight={innerHeight} setLanded={setLanded} />
+    // bodyContent = <Landing setLanded={setLanded} />
   }
 
   return (
     // <div className="container">{bodyContent}</div>
-    // <div className="container" style={{ height: props.innerHeight, width: props.innerWidth}}>{bodyContent}</div>
-    <div className="container">{bodyContent}</div>
+    <div className="container" style={{ height: innerHeight, width: innerWidth}}>{bodyContent}</div>
+    // <div className="container">{bodyContent}</div>
   )
 }
