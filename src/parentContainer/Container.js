7a90500ac62845a7b48fb9bbb33b78a6f9a675a8
c2kle <c2kle@live.unc.edu>
2022-11-12 22:27:42 -0500

    viewport responsive

--- src/parentContainer/Container.js
+++ src/parentContainer/Container.js
@@ -3,47 +3,21 @@ import '../parentContainerStyles/container.css'
 import Main from './Main';
 import Landing from '../pages/Landing';
 import { useState } from 'react';
-// import { useDebouncedCallback } from 'use-debounce'
 
 export default function Container() {
 
-
-
-  const [innerHeight, setInnerHeight] = useState(window.innerHeight)
   const [landed,setLanded] = useState(false)
-
-  //innerWidth bug, can use outerWidth since browser toolbars are vertical
-  
-
-  // const debouncedVersion = useDebouncedCallback(() => {
-  //   setInnerHeight(window.innerHeight)
-  // }, 20)
-
-  // window.addEventListener('resize', debouncedVersion)
-
-
-  // window.addEventListener('orientationchange', debouncedVersion)
-
-  window.addEventListener('resize', () => {
-    setInnerHeight(window.innerHeight)
-  })
-
-  window.addEventListener('orientationchange', () => {
-    setInnerHeight(window.innerHeight)
-  })
-
 
   let bodyContent;
 
   if (landed === true) {
-    bodyContent = <Main innerHeight={innerHeight} />
+    bodyContent = <Main />
   }
   else {
-    bodyContent = <Landing innerHeight={innerHeight} setLanded={setLanded} />
+    bodyContent = <Landing setLanded={setLanded} />
   }
 
   return (
-    // <div className="container">{bodyContent}</div>
-    <div className="container" style={{ height: innerHeight}}>{bodyContent}</div>
+    <div className="container" >{bodyContent}</div>
   )
 }
